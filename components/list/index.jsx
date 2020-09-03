4469dea8ce8dab698d6d68e5ff9e75cb26002590
Justin Tappert <tappertwj@gmail.com>
2020-09-03 08:31:35 -0700

    subscribing to list change events

--- components/list/index.jsx
+++ components/list/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import {Avatar, List, Space} from "antd";
+import {Avatar, Button, List, Space} from "antd";
 import { MessageOutlined, LikeOutlined, StarOutlined } from '@ant-design/icons';
 
 const IconText = ({ icon, text }) => (
@@ -10,58 +10,31 @@ const IconText = ({ icon, text }) => (
   </Space>
 );
 
-export default function Wishlist({ wishlist }) {
+export default function Wishlist({ wishlist, handleAddItem }) {
   return (
-    <List
-      itemLayout="horizontal"
-      size="large"
-      dataSource={wishlist}
-      pagination={{
-        onChange: page => console.log({page}),
-        pageSize: 2
-      }}
-      renderItem={item => (
-      <List.Item key={item.name} actions={[
-        <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
-        <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
-        <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
-      ]}>
-        <List.Item.Meta
-          avatar={<Avatar shape="square" size={100} src={item.image} />}
-          title={<Link href=""><a>{item.name}</a></Link>}
-          description={`Price: ${item.price} - Category: ${item.category}/${item.subcategory}`} />
-      </List.Item>
-    )}>
-    </List>
+    <React.Fragment>
+    <Button onClick={() => handleAddItem()}>Add Item</Button>
+      <List
+        itemLayout="horizontal"
+        size="large"
+        dataSource={wishlist}
+        pagination={{
+          onChange: page => console.log({page}),
+          pageSize: 50
+        }}
+        renderItem={item => (
+        <List.Item key={item.name} actions={[
+          <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
+          <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
+          <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
+        ]}>
+          <List.Item.Meta
+            avatar={<Avatar shape="square" size={100} src={item.image} />}
+            title={<Link href=""><a>{item.name}</a></Link>}
+            description={`Price: ${item.price} - Category: ${item.category}/${item.subcategory}`} />
+        </List.Item>
+      )}>
+      </List>
+    </React.Fragment>
   )
 }
-const data = [
-  {
-    name: 'SVG Animations: From Common UX Implementations to Complex Responsive Animation',
-    image: 'https://m.media-amazon.com/images/I/51iV+wxhw9L._SS135_.jpg',
-    price: 42.99,
-    category: 'book',
-    subcategory: 'technology'
-  },
-  {
-    name: 'Rich Dad Poor Dad: What the Rich Teach Their Kids About Money That the Poor and Middle Class Do Not!',
-    image: 'https://m.media-amazon.com/images/I/51wOOMQ+F3L._SS135_.jpg',
-    price: 5.39,
-    category: 'book',
-    subcategory: 'finance'
-  },
-  {
-    name: "The Boggleheads' Guide to Investing" ,
-    image: 'https://m.media-amazon.com/images/I/51hIN5N4BlL._SS135_.jpg',
-    price: 14.39,
-    category: 'book',
-    subcategory: 'finance'
-  },
-  {
-    name: 'Microsoft Sculpt Ergonomic Keyboard for Business',
-    image: 'https://m.media-amazon.com/images/I/31dAfO9Ge1L._SS135_.jpg',
-    price: 66.43,
-    category: 'electronics',
-    subcategory: 'keyboard'
-  },
-];
--- pages/index.js
+++ pages/index.js
@@ -5,27 +5,44 @@ import Wishlist from "components/list";
 
 export default function Home() {
   const firebase = useContext(FirebaseContext);
-  const [wishlist, setWishlist] = useState(null);
+  const [wishlist, setWishlist] = useState([]);
 
   // this is a wishlist guid
-  const ref = firebase.database().ref(`wishlists/4f08dedf-dd17-4327-9da3-121abef7c53a`)
+  const listRef = firebase.database().ref(`wishlists/4f08dedf-dd17-4327-9da3-121abef7c53a`)
 
   useEffect(() => {
-    ref.once('value').then((snapshot) => {
+    listRef.once('value').then((snapshot) => {
       if (!snapshot) {
         setWishlist([]);
       } else {
         const tempList = [];
-        snapshot.forEach(item => {
-          tempList.push({ ...item.val() })
-          setWishlist(tempList);
-        })
+        snapshot.forEach((item, index) => {
+          tempList.push({ key: index,  ...item.val() })
+        });
+        setWishlist(tempList);
       }
     }).catch((err) => console.error(err));
   }, []);
 
+  useEffect(() => {
+    listRef.on('child_added', (data) => {
+      setWishlist(prevState => [...prevState, data.val() ]);
+    });
+  }, [firebase]);
+
+  function addItem() {
+    // const newListItem = listRef.push();
+    // newListItem.set({
+    //   name: 'SVG Animations: From Common UX Implementations to Complex Responsive Animation',
+    //   image: 'https://m.media-amazon.com/images/I/51iV+wxhw9L._SS135_.jpg',
+    //   price: 42.99,
+    //   category: 'book',
+    //   subcategory: 'technology'
+    // });
+  }
+
   return <div>
    <h1>Hello World</h1>
-    <Wishlist wishlist={wishlist} />
+    <Wishlist wishlist={wishlist} handleAddItem={addItem} />
   </div>
 }
