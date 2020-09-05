ef7ef7b3e354823eb1a14eea3603ab0874fe1ce3
Justin Tappert <tappertwj@gmail.com>
2020-09-05 13:50:05 -0700

    style cleanup

--- components/profile/profile-lists.jsx
+++ components/profile/profile-lists.jsx
@@ -7,7 +7,7 @@ const {Header, Sider, Content} = Layout;
 
 export default function ProfileList() {
   return (
-    <Tabs defaultActiveKey="1" style={{height: '85%'}}>
+    <StyledTabs defaultActiveKey="1" style={{height: '85%'}}>
       <TabPane tab="Your Wishlists" key="1" style={{height: '100%'}}>
         <Layout style={{height: '100%'}}>
           <StyledSider>Sider</StyledSider>
@@ -20,10 +20,15 @@ export default function ProfileList() {
       <TabPane tab="Your Friend's Wishlists" key="2">
         Content of Tab Pane 2
       </TabPane>
-    </Tabs>
+    </StyledTabs>
   )
 }
 
+const StyledTabs = styled(Tabs)`
+  div.ant-tabs-content {
+    height: 100%;
+  }
+`;
 const StyledHeader = styled(Header)`
   background: transparent;
   border: 1px solid black;
