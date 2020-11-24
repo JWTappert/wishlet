2b00a7c31da0189aeb4ad371057e2f727b3c87d8
Justin Tappert <tappertwj@gmail.com>
2020-11-24 15:28:26 -0800

    use url for fetching user

--- contexts/user-context.js
+++ contexts/user-context.js
@@ -1,9 +1,12 @@
 import React, { createContext } from "react";
+
 import UserState from "stores/user";
 import * as AWS from "utils/aws/user";
+import useQueryParam from "../hooks/use-query-param";
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  return <UserContext.Provider value={new UserState(AWS, {username: '47e12680-8af6-4d05-98ae-3223d057603b'})}>{children}</UserContext.Provider>;
+  const uid = useQueryParam("uid");
+  return <UserContext.Provider value={new UserState(AWS, { username: uid })}>{children}</UserContext.Provider>;
 };
