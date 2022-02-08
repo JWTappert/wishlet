200d729d6cca2b05cc840e682c9c0270a0585244
JWTappert <tappertwj@gmail.com>
2022-02-07 21:05:49 -0800

    adding grommet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Box, Grommet } from "grommet";
+
+const theme = {
+  global: {
+    font: {
+      family: "Roboto",
+      size: "14px",
+      height: "20px",
+    },
+  },
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Grommet theme={theme}>
+      <Box margin={{ top: "smallish" }}>
+        <Component {...pageProps} />
+      </Box>
+    </Grommet>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
+      <p>test</p>
     </div>
   );
 }
